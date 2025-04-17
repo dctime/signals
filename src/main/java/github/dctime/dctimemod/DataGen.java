ab76bf1729bb1cdfd590615b6c261f7edd2c556e
DCtime <daniel.chu916@gmail.com>
2025-04-17 18:13:50 +0800

    Build Helper Block Loot Table Added use runData to generate json files

---
+++ src/main/java/github/dctime/dctimemod/DataGen.java
@@ -0,0 +1,37 @@
+package github.dctime.dctimemod;
+
+import com.nimbusds.jose.util.Resource;
+import github.dctime.dctimemod.block.BlockLootSubProvider;
+import github.dctime.dctimemod.block.RegisterBlocks;
+import net.minecraft.core.Registry;
+import net.minecraft.core.RegistrySetBuilder;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.loot.LootTableProvider;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.storage.loot.LootTable;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.data.event.GatherDataEvent;
+
+import java.util.List;
+import java.util.Set;
+
+@EventBusSubscriber(modid=DCtimeMod.MODID, bus= EventBusSubscriber.Bus.MOD)
+public class DataGen {
+    @SubscribeEvent
+    public static void onGatherData(GatherDataEvent.Client event) {
+        event.createProvider(((packOutput, completableFuture) -> new LootTableProvider(
+                packOutput,
+                Set.of(),
+                List.of(new LootTableProvider.SubProviderEntry(
+                        BlockLootSubProvider::new,
+                        LootContextParamSets.BLOCK
+                )),
+                completableFuture
+        )));
+    }
+}
---
+++ src/main/java/github/dctime/dctimemod/block/BlockLootSubProvider.java
@@ -0,0 +1,27 @@
+package github.dctime.dctimemod.block;
+
+import net.minecraft.core.HolderLookup;
+import net.minecraft.world.flag.FeatureFlagSet;
+import net.minecraft.world.flag.FeatureFlags;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.block.Block;
+
+import java.util.Set;
+
+public class BlockLootSubProvider extends net.minecraft.data.loot.BlockLootSubProvider {
+    public BlockLootSubProvider(HolderLookup.Provider lookupProvider) {
+        super(Set.of(), FeatureFlags.DEFAULT_FLAGS, lookupProvider);
+    }
+
+    @Override
+    protected Iterable<Block> getKnownBlocks() {
+        return RegisterBlocks.BLOCKS.getEntries().stream().map(e -> (Block)e.value()).toList();
+    }
+
+    @Override
+    protected void generate() {
+        this.add(RegisterBlocks.BUILD_HELPER_BLOCK.get(), this.createSingleItemTable(Items.COBBLESTONE));
+
+    }
+}
